9dbea6f3857f80a5f89080aaaca47c17d3058da2
Xznder1984 <168613623+Xznder1984@users.noreply.github.com>
2025-05-13 14:13:20 +0700

    just double click on them
    and if you want to download all of them press code and press download with zip

---
+++ Viruses/MATRIX.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:1
+color a
+echo %random%%random%%random%%random%%random%%random% %random%%random%%random%%random% %random%%random%%random%%random% %random%%random%
+:2
+color b
+echo %random%%random%%random%%random%%random%%random% %random%%random%%random%%random% %random%%random%%random%%random% %random%%random%
+:3
+color c
+echo %random%%random%%random%%random%%random%%random% %random%%random%%random%%random% %random%%random%%random%%random% %random%%random%
+goto 1
